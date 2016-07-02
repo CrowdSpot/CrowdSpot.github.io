59f7de5989c1731a20204330fdb3eb7ae9e7fc7b
finnbuster <finnbuster@gmail.com>
2016-07-02 18:38:16 +1000

    trying fix for features waypoints

--- js/site.js
+++ js/site.js
@@ -147,38 +147,7 @@ $(function() {
 		offset: '-40%'
 	});
 
-	var waypoint = new Waypoint({
-		element: $('.crowdspot-features__list'),
-		handler: function(direction) {
-			if (direction === 'down') {
-				// $('#moving-phone').addClass('fixer');
-				// console.log('stick it!');
-				$('.crowdspot-features__images__container').addClass('fixer');
-			} else {
-				// $('#moving-phone').removeClass('fixer').removeClass('slide-send-off').removeClass('slide-withdraw-off');
-				$('.crowdspot-features__images__container').removeClass('fixer');
-			}
-		},
-		offset: 200
-	});
-
-	var waypointStop = new Waypoint({
-		element: $('.crowdspot-features__list'),
-		handler: function(direction) {
-			if (direction === 'down') {
-				$('.crowdspot-features__images__container').removeClass('fixer').addClass('stop');
-			} else {
-				$('.crowdspot-features__images__container').removeClass('stop').addClass('fixer');
-			}
-		},
-		offset: function() {
-			// console.log('images height', $('.crowdspot-features__images__container img').height());
-			// console.log('features list height',$('.crowdspot-features__list').height());
-			// console.log('window height', $(window).height());
-			// return -(this.element[0].clientHeight - $('.crowdspot-features__images__container').height());
-			return - ( $('.crowdspot-features__list').height() - $('.crowdspot-features__images__container img').height() - 200 );
-		}
-	});
+	
 
 	$(window).resize(function(event) {
 		Waypoint.refreshAll();
@@ -539,6 +508,39 @@ $(function() {
 		});
 
 		preloadImages(imagesToPreload);
+
+		var waypoint = new Waypoint({
+			element: $('.crowdspot-features__list'),
+			handler: function(direction) {
+				if (direction === 'down') {
+					// $('#moving-phone').addClass('fixer');
+					// console.log('stick it!');
+					$('.crowdspot-features__images__container').addClass('fixer');
+				} else {
+					// $('#moving-phone').removeClass('fixer').removeClass('slide-send-off').removeClass('slide-withdraw-off');
+					$('.crowdspot-features__images__container').removeClass('fixer');
+				}
+			},
+			offset: 200
+		});
+
+		var waypointStop = new Waypoint({
+			element: $('.crowdspot-features__list'),
+			handler: function(direction) {
+				if (direction === 'down') {
+					$('.crowdspot-features__images__container').removeClass('fixer').addClass('stop');
+				} else {
+					$('.crowdspot-features__images__container').removeClass('stop').addClass('fixer');
+				}
+			},
+			offset: function() {
+				// console.log('images height', $('.crowdspot-features__images__container img').height());
+				// console.log('features list height',$('.crowdspot-features__list').height());
+				// console.log('window height', $(window).height());
+				// return -(this.element[0].clientHeight - $('.crowdspot-features__images__container').height());
+				return - ( $('.crowdspot-features__list').height() - $('.crowdspot-features__images__container img').height() - 200 );
+			}
+		});
 
 	});
 
